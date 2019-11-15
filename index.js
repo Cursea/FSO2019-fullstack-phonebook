acd70b94756723aa98ae04569a38e3b4318e0505
Marco <marcoburman@gmail.com>
2019-11-15 01:56:46 +0000

    add 3.1: Phonebook backend step1; setup app/persons

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-1168101",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-561565",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-68448",
+    id: 3
+  }
+];
+
+app.use(bodyParser.json());
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
